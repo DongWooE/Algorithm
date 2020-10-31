f07308ea3fe534deed1b9d8bd2d297224da9d468
DongWooE <63181439+DongWooE@users.noreply.github.com>
2020-11-01 02:11:31 +0900

    Add files via upload

--- Bellman_Ford.cpp
+++ Bellman_Ford.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
 
-	int n, m;
+	int n, m; 
 	cout << " input number of vertices and edges : ";
 	cin >> n >> m;
 
@@ -17,31 +17,29 @@ int main() {
 	for (int i = 0; i < m; i++) {
 		int t1, t2, weight;
 		cin >> t1 >> t2 >> weight;
-		E[t1].push_back(make_pair(t2, weight));
+		E[t1].push_back(make_pair(t2, weight));  //digraph
 	}
 
 
 	int limit;
 	limit = numeric_limits<int>::max();
 
-	vector<int>dist(n, limit);  //무한대 값을 넣어줌
-	vector<int>prev(n, -1);  //nil로 초기화
-
-
+	vector<int>dist(n, limit);  //array dist is that will show you the shortest path's cost
+	vector<int>prev(n, -1);  //remember what vertex update the distance
 
 	int start;  //starting node
 	
-	cout << "Input starting node (that dosen't make cycle) : ";  
+	cout << "Input starting node : ";  
 	cin >> start;
 
-	dist[start] = 0;
+	dist[start] = 0;  //set the start node's distance 0
 	
 	int count = 0;
-	while (count <= (n - 1)) {
+	while (count <= (n - 1)) {  //repeat (n-1) times
 		count++;
-		for (int v = 0; v < n; v++) {
+		for (int v = 0; v < n; v++) {  //for all edges in E
 			for (int i = 0; i < E[v].size(); i++) {
-				if (dist[v] != limit) {
+				if (dist[v] != limit) {  //if the current node's cost is limit , then pass
 					if (dist[E[v][i].first] > dist[v] + E[v][i].second) {
 						dist[E[v][i].first] = dist[v] + E[v][i].second;
 						prev[E[v][i].first] = v;
@@ -51,7 +49,7 @@ int main() {
 		}
 	}
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {  //print the distance
 		cout << " " << dist[i] << " ";
 	}
 }
